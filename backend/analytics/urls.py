221fe560ad74904dc33c03c5813b3ffb311f6796
Aleksey Eremeev <eremeev.dev@gmail.com>
2016-05-28 01:30:15 +0300

    add api endpoint for retrieve auth token

--- backend/analytics/urls.py
+++ backend/analytics/urls.py
@@ -1,8 +1,12 @@
 from django.conf.urls import url
+
+from rest_framework.authtoken import views
+
 from analytics.views import InstaUserListCreateAPIView, InstaUserDestroyAPIView, StatListAPIView
 
 urlpatterns = [
     url(r'^insta_users/$', InstaUserListCreateAPIView.as_view()),
     url(r'^insta_users/(?P<pk>\w+)/$', InstaUserDestroyAPIView.as_view()),
     url(r'^insta_users/(?P<pk>\w+)/stat/$', StatListAPIView.as_view()),
+    url(r'^token_auth/$', views.obtain_auth_token),
 ]
