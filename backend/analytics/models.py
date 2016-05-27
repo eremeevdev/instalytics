9128a4e0914c33a9a98f944a3904fb9e3df6f64e
Aleksey Eremeev <eremeev.dev@gmail.com>
2016-05-27 22:38:55 +0300

    create models and migrations

--- backend/analytics/models.py
+++ backend/analytics/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    id = models.IntegerField(primary_key=True)
+    username = models.CharField(max_length=64)
+
+
+class Stat(models.Model):
+    followed_by = models.IntegerField()
+    follows = models.IntegerField()
+    media_count = models.IntegerField()
