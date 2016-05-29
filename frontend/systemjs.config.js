b40facdf5fb0dbd63c5519fd468963a3b666c053
Aleksey Eremeev <eremeev.dev@gmail.com>
2016-05-29 03:18:39 +0300

    recreate frontend

--- frontend/systemjs.config.js
+++ frontend/systemjs.config.js
@@ -1,21 +1,25 @@
 /**
- * System configuration for Angular 2 samples
+ * System configuration for Angular 2 apps
  * Adjust as necessary for your application needs.
  */
 (function(global) {
+
   // map tells the System loader where to look for things
   var map = {
     'app':                        'app', // 'dist',
+
     '@angular':                   'node_modules/@angular',
     'angular2-in-memory-web-api': 'node_modules/angular2-in-memory-web-api',
     'rxjs':                       'node_modules/rxjs'
   };
+
   // packages tells the System loader how to load when no filename and/or no extension
   var packages = {
     'app':                        { main: 'main.js',  defaultExtension: 'js' },
     'rxjs':                       { defaultExtension: 'js' },
-    'angular2-in-memory-web-api': { defaultExtension: 'js' },
+    'angular2-in-memory-web-api': { main: 'index.js', defaultExtension: 'js' },
   };
+
   var ngPackageNames = [
     'common',
     'compiler',
@@ -27,13 +31,27 @@
     'router-deprecated',
     'upgrade',
   ];
-  // Add package entries for angular packages
-  ngPackageNames.forEach(function(pkgName) {
+
+  // Individual files (~300 requests):
+  function packIndex(pkgName) {
+    packages['@angular/'+pkgName] = { main: 'index.js', defaultExtension: 'js' };
+  }
+
+  // Bundled (~40 requests):
+  function packUmd(pkgName) {
     packages['@angular/'+pkgName] = { main: pkgName + '.umd.js', defaultExtension: 'js' };
-  });
+  };
+
+  var setPackageConfig = System.packageWithIndex ? packIndex : packUmd;
+
+  // Add package entries for angular packages
+  ngPackageNames.forEach(setPackageConfig);
+
   var config = {
     map: map,
     packages: packages
   }
+
   System.config(config);
+
 })(this);
