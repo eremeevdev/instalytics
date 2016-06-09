73607b9ff1f2abb3506e96d0d8069ab3172d59f8
Aleksey Eremeev <eremeev.dev@gmail.com>
2016-06-10 02:59:01 +0300

    show list of users

--- frontend/app/users.component.ts
+++ frontend/app/users.component.ts
@@ -3,6 +3,7 @@ import { Router } from '@angular/router-deprecated';
 import { ApiService } from './api.service';
 import { TokenService } from './token.service';
 import { UserAddComponent } from './user-add.component';
+import { InstaUser } from './insta-user';
 
 @Component({
     selector: 'users',
@@ -13,11 +14,13 @@ export class UsersComponent implements OnInit {
 
     constructor(private api: ApiService, private token: TokenService, private router: Router) { }
 
+    users: InstaUser[];
+
     ngOnInit() {
 
         this.api.get('/api/insta_users/')
             .then((response) => {
-                console.log(response);
+                this.users = response.json();
             })
             .catch((err) => {
                 console.log(err);
