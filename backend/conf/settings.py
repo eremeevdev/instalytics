d9d11389bad4d268fbd15c376bd196ad94c42cf3
Aleksey Eremeev <eremeev.dev@gmail.com>
2016-06-14 23:54:55 +0300

    config django for postgres

--- backend/conf/settings.py
+++ backend/conf/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.postgres',
     'rest_framework',
     'rest_framework.authtoken',
     'analytics',
@@ -79,8 +80,11 @@ WSGI_APPLICATION = 'conf.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_ENV_POSTGRES_DB'),
+        'USER': os.environ.get('DB_ENV_POSTGRES_USER'),
+        'PASSWORD': os.environ.get('DB_ENV_POSTGRES_PASSWORD'),
+        'HOST': os.environ.get('DB_PORT_5432_TCP_ADDR'),
     }
 }
 
