f8dbd302c6a0a7819b5d28dca79b0fc0fcc52060
Aleksey Eremeev <eremeev.dev@gmail.com>
2016-06-15 00:37:51 +0300

    register ChangeLog model in admin

--- backend/analytics/admin.py
+++ backend/analytics/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from analytics.models import InstaUser, Stat
+from analytics.models import InstaUser, Stat, ChangeLog
 
 admin.site.register(InstaUser)
 admin.site.register(Stat)
+admin.site.register(ChangeLog)
