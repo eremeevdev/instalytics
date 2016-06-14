806740722b018c42a97679f63465559d8e2e42f3
Aleksey Eremeev <eremeev.dev@gmail.com>
2016-06-15 00:33:28 +0300

    fix management command for update stat and info

--- backend/analytics/management/commands/update_stat.py
+++ backend/analytics/management/commands/update_stat.py
@@ -1,5 +1,5 @@
 from django.core.management.base import BaseCommand
-from analytics.stat import update_stat
+from analytics.stat import update_info
 
 
 class Command(BaseCommand):
@@ -7,4 +7,4 @@ class Command(BaseCommand):
     help = "Update user's instagram stat"
 
     def handle(self, *args, **kwargs):
-        update_stat()
+        update_info()
